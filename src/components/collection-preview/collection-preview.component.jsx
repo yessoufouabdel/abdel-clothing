d733dbbe918deeee56a040ef4cdb04176c6a5325
Abdel Yessoufou <abdel.yessoufou@softwaregroup-bg.com>
2021-02-10 14:25:43 +0000

    firebase implementation

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
-import CollectionItem from "../collection-item/collection-item.component";
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-        {
-            items.map(({id, ...otherItemProps})=>(
-                <CollectionItem key={id} {...otherItemProps} />
-            ))
-        }
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,49 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAnAQyDNOiHhre5Xh2lCZhjdWILb0NJp10",
+    authDomain: "abdel-clothing-db.firebaseapp.com",
+    projectId: "abdel-clothing-db",
+    storageBucket: "abdel-clothing-db.appspot.com",
+    messagingSenderId: "860651134802",
+    appId: "1:860651134802:web:24216bc0a7d8ec94c7248b",
+    measurementId: "G-SWM30CXFLT"
+  };
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
