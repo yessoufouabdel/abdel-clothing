86b03659695eb5957daa79753f10494ac34e4aa4
Abdel Yessoufou <abdel.yessoufou@softwaregroup-bg.com>
2021-02-15 11:13:42 +0000

    Added checkout page, checkout component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-import {setCurrentUser} from './redux/user/user.actions';
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
@@ -41,30 +46,31 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route 
-            exact 
-            path='/signin' 
-            render = {() =>
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
               this.props.currentUser ? (
-                <Redirect to = '/' />
-              ):(
-                <SignInAndSignUpPage/>
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
               )
             }
-            />
+          />
         </Switch>
       </div>
     );
   }
-};
+}
 
-const mapStateToProps = ({user}) =>({
-  currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
